296669232729669fc9a6128eac9820ea2539bd6f
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-10-14 15:01:51 +0500

    updated

---
+++ src/app/componants/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function footer() {
+  return (
+    <div className="grid  border-t-4 border-double border-black place-items-center font-custom tracking-widest">
+      <div className=" mt-5 text-xl text-[#0079ac]">
+        © 2024 by Great Gonzo. All rights reserved!
+      </div>
+    </div>
+  );
+}
---
+++ src/app/lib/client.js
@@ -0,0 +1,5 @@
+// import { createThirdwebClient } from "thirdweb";
+
+// export const client = createThirdwebClient({
+//     clientId: "d75935c2db33dd391882dd2fb2474ceb",
+// });
